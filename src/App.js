87f133446b05f8209fba6f558c1833ccba67789a
dpeek2010 <dpeek2010@yahoo.com>
2024-02-05 12:41:28 -0600

    added delete functionality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import Editor from "./components/Editor";
 import { data } from "./data";
-import Split from "split.js";
+import Split from "react-split";
 //import Split from "@uiw/react-split";
 import { nanoid } from "nanoid";
 
 export default function App() {
-  const [notes, setNotes] = React.useState([]);
+  //initalize state by accessing local storage, get item with key of "notes"
+  const [notes, setNotes] = React.useState(
+    () => JSON.parse(localStorage.getItem("notes")) || []
+  );
   const [currentNoteId, setCurrentNoteId] = React.useState(
     (notes[0] && notes[0].id) || ""
   );
+
+  React.useEffect(() => {
+    //update the notes key inside of local storage
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   function createNewNote() {
     const newNote = {
@@ -23,13 +31,37 @@ export default function App() {
   }
 
   function updateNote(text) {
-    setNotes((oldNotes) =>
+    // move the most recently-modified note to the top of the list
+    setNotes((oldNotes) => {
+      //create new empty array
+      const newArray = [];
+      //loop over original.
+      for (let i = 0; i < oldNotes.length; i++) {
+        const oldNote = oldNotes[i];
+        //If ID matches, put updated note at the beginning of the new array.
+        if (oldNote.id === currentNoteId) {
+          newArray.unshift({ ...oldNote, body: text });
+        } else {
+          //Else push old note to end of array
+          newArray.push(oldNote);
+        }
+      }
+      //return the new array
+      return newArray;
+    });
+    // This does not rearrange the notes
+    /* setNotes((oldNotes) =>
       oldNotes.map((oldNote) => {
         return oldNote.id === currentNoteId
           ? { ...oldNote, body: text }
           : oldNote;
       })
-    );
+    ); */
+  }
+
+  function deleteNote(event, noteId) {
+    event.stopPropagation();
+    setNotes((oldNotes) => oldNotes.filter((note) => note.id !== noteId));
   }
 
   function findCurrentNote() {
@@ -49,6 +81,7 @@ export default function App() {
             currentNote={findCurrentNote()}
             setCurrentNoteId={setCurrentNoteId}
             newNote={createNewNote}
+            deleteNote={deleteNote}
           />
           {currentNoteId && notes.length > 0 && (
             <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
